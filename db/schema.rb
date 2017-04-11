76daf323738076eec46dd7fbe61d697e27ce4e97
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2017-04-11 23:25:41 +0900

    Created seeds file for the sample data

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20170411133055) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "skills", force: :cascade do |t|
     t.string   "title"
     t.integer  "percent_utilized"
